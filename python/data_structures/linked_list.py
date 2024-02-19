055c5260f2d71b17dbc83504ef8c40bf11d53558
lookaside <123149780+chariotsofiron@users.noreply.github.com>
2024-02-19 16:47:53 -0600

    add linked list data structure

---
+++ python/data_structures/linked_list.py
@@ -0,0 +1,43 @@
+"""Linked list."""
+from __future__ import annotations
+
+from typing import Iterator
+
+
+class ListNode:
+    """Linked list node."""
+
+    def __init__(self, data: int, next_node: ListNode | None = None) -> None:
+        """Constructs a node with the given data and next node."""
+        self.data = data
+        self.next = next_node
+
+    @classmethod
+    def from_list(cls: type[ListNode], items: list[int]) -> ListNode:
+        """Constructs a linked list from a list of integers."""
+        if not items:
+            raise ValueError
+        head = cls(items[0])
+        current = head
+        for item in items[1:]:
+            current.next = cls(item)
+            current = current.next
+        return head
+
+    def __str__(self) -> str:
+        """Returns a string representation of the list."""
+        return "->".join(str(x) for x in self)
+
+    def __iter__(self) -> Iterator[int]:
+        """Iterates over the list."""
+        current = self
+        while current:
+            yield current.data
+            current = current.next
+
+
+def test() -> None:
+    """Run tests."""
+    head = ListNode.from_list([1, 2, 3])
+    assert str(head) == "1->2->3"
+    assert list(head) == [1, 2, 3]
