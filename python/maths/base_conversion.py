4592a1e3b949b71e3cafb3d9bda399d675df9114
lookaside <123149780+chariotsofiron@users.noreply.github.com>
2024-02-19 18:00:20 -0600

    add base conversion

---
+++ python/maths/base_conversion.py
@@ -0,0 +1,27 @@
+"""Base conversion.
+
+- <https://bradfieldcs.com/algos/stacks/converting-number-bases/>
+"""
+import pytest
+
+SYMBOLS = "0123456789abcdefghijklmnopqrstuvwxyz"
+
+
+def convert_to_base(number: int, base: int) -> str:
+    """Converts a decimal number to a different base."""
+    remainder_stack = []
+    while number > 0:
+        remainder = number % base
+        remainder_stack.append(remainder)
+        number = number // base
+
+    new_digits = (SYMBOLS[digit] for digit in reversed(remainder_stack))
+    return "".join(new_digits)
+
+
+@pytest.mark.parametrize(
+    ("number", "base", "expected"), [(25, 2, "11001"), (25, 16, "19")]
+)
+def test(number: int, base: int, expected: str) -> None:
+    """Runs test cases."""
+    assert convert_to_base(number, base) == expected
